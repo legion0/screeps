8977759d74153ba492fb4d6d8f36b65683a3b8a4
legion0 <phenixdoc@gmail.com>
2016-06-12 11:56:11 +0300

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,18 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('constants');
+ * mod.thing == 'a thing'; // true
+ */
+
+module.exports = {
+    EVENT_TICK_END: 'EVENT_TICK_END',
+    EVENT_TICK_START: 'EVENT_TICK_START',
+    EVENT_ROOM_DISCOVERED: 'EVENT_ROOM_DISCOVERED',
+    ROLE_HARVESTER: 'HARVESTER',
+    ROLE_BUILDER: 'BUILDER',
+    ROLE_UPGRADER: 'UPGRADER',
+    ROLE_MULE: 'MULE'
+};
