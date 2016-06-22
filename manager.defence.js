f607b620729b324c8424717d99a3d306a00eb305
Jonathan Kibarsky <phenixdoc@gmail.com>
2016-06-22 22:20:34 +0300

    tower focus fire

--- manager.defence.js
+++ manager.defence.js
@@ -1,20 +1,16 @@
-var CONSTANTS = require('constants');
-var events = require('events');
-
-events.listen(CONSTANTS.EVENT_ROOM_DISCOVERED, (event_name, roomName) => {
-    // console.log('DefenceManager', 'EVENT_ROOM_DISCOVERED', roomName);
-    var room = Game.rooms[roomName];
-    var room_memory = Memory.rooms[roomName];
-    var memory = room_memory.manager_defence = {};
-
-    memory.attack_in_progress = false;
-
-    return true;
-});
+// var CONSTANTS = require('constants');
+// var events = require('events');
 
 function DefenceManager(room) {
     this.room = room;
+    this.room_memory = room.memory;
     this.memory = room.memory.manager_defence;
+    if (!this.memory) {
+        this.memory = room.memory.manager_defence = {};
+    }
+    if (this.room_memory.attack_in_progress === undefined) {
+        this.room_memory.attack_in_progress = false;
+    }
 }
 DefenceManager.prototype.run = function() {
     var room = this.room;
@@ -25,30 +21,32 @@ DefenceManager.prototype.run = function() {
         return;
     }
     var hostile_creeps = room.find(FIND_HOSTILE_CREEPS);
-    if (hostile_creeps.length || memory.attack_in_progress) {
-        if (!memory.attack_in_progress) {
+    if (hostile_creeps.length) {
+        if (!this.room_memory.attack_in_progress) {
             console.log(Game.time, 'Enemy combatants discovered, initiating defensive actions');
-            memory.attack_in_progress = true;
+            this.room_memory.attack_in_progress = true;
+        }
+    }
+    if (this.room_memory.attack_in_progress) {
+        if (!this.room_memory.attack_in_progress) {
+            this.room_memory.attack_in_progress = true;
         }
         var hostiles = room.findHostileStructures().concat(hostile_creeps);
         if (!hostiles.length) {
-            memory.attack_in_progress = false;
+            console.log(Game.time, 'Enemy combatants destroyed, disabling defensive actions');
+            this.room_memory.attack_in_progress = false;
             return;
         }
         var towers = room.findTowers();
         towers.forEach((tower) => {
-            var hostile = tower.pos.findClosestHostile();
-            if (hostile) {
-                // console.log(Game.time, tower, '=>', hostile);
-                var attack_ret_val = tower.attack(hostile);
-                if ([OK, ERR_NOT_ENOUGH_ENERGY].indexOf(attack_ret_val) == -1) {
-                    console.log(Game.time, 'ERROR ERROR ERROR ERROR ERROR', 'Unexpected tower attack_ret_val', attack_ret_val);
-                }
+            // var hostile = tower.pos.findClosestHostile();
+            var hostile = hostiles[0];
+            // console.log(Game.time, tower, '=>', hostile);
+            var attack_ret_val = tower.attack(hostile);
+            if ([OK, ERR_NOT_ENOUGH_ENERGY].indexOf(attack_ret_val) == -1) {
+                console.log(Game.time, 'ERROR ERROR ERROR ERROR ERROR', 'Unexpected tower attack_ret_val', attack_ret_val);
             }
         });
-    } else if (memory.attack_in_progress) {
-        console.log(Game.time, 'Enemy combatants destroyed, disabling defensive actions');
-        memory.attack_in_progress = false;
     }
 
 }
