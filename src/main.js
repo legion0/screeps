e68487593a51c9b730542650f82a5782b0d9d264
Jonathan Kibarsky <phenixdoc@gmail.com>
2016-07-16 01:00:45 +0300

    basic hardcoded attack to flag

--- src/main.js
+++ src/main.js
@@ -18,9 +18,37 @@ var WorkforceManager = require('manager.workforce');
 var DefenceManager = require('manager.defence');
 var AttackManager = require('manager.Attack');
 
+function attack_flag() {
+
+    let flag = Game.flags.attack;
+    if (!flag) {
+        return;
+    }
+    if (!Game.creeps.a) {
+        Game.spawns.Spawn1.createCreep([MOVE, ATTACK, MOVE, ATTACK, MOVE, ATTACK, MOVE, ATTACK], 'a');
+        return;
+    }
+
+    let creep = Game.creeps.a;
+    if (creep.pos.getRangeTo(flag) != 0) {
+        creep.moveTo(flag);
+    } else {
+        let structure = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+        if (structure && structure.pos.getRangeTo(creep) <= 1) {
+            creep.attack(structure);
+        } else {
+            let enemy_creeps = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            creep.attack(enemy_creeps);
+            
+        }
+    }
+}
+
 module.exports.loop = function () {
     PathFinder.use(true);
     try {
+        attack_flag();
+
         if (Game.time % 10 == 0) {
             // console.log(Game.time, 'Checking for new rooms.');
             for (var roomName in Game.rooms) {
